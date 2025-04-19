f0215582dc0ba25f3078cfee94b1a7db5ced65ae
nehir-mentes <nehirmen.98@gmail.com>
2025-04-19 21:51:54 +0000

    firstsymbol

--- app.rb
+++ app.rb
@@ -26,3 +26,27 @@ get("/") do
   # Render a view template
   erb(:homepage)
 end
+
+# Build the target
+get ("/:first_symbol") do
+
+  @the_symbol = params.fetch("first_symbol")
+
+    # Assemble the API url, including the API key in the query string
+    api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+
+    # Use HTTP.get to retrieve the API data
+    @raw_response = HTTP.get(api_url)
+  
+    # Get the body of the response as a string
+    @raw_string = @raw_response.to_s
+  
+    # Convert the string to JSON
+    @parsed_data = JSON.parse(@raw_string)
+  
+    # View the currencies
+    @currencies = @parsed_data.fetch("currencies")
+  
+    # Render a view template
+    erb (:target_currency)
+end
